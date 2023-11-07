27f5430330c38fe118539244c9eeb684795e0df0
UlasBingol <e203316@metu.edu.tr>
2023-11-07 16:35:17 +0100

    Solved norm-denorm issues.

--- extractor.py
+++ extractor.py
@@ -3,47 +3,74 @@ import numpy as np
 
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
+
+from skimage.transform import AffineTransform
+
+#turn [x,y] -> [x,y,1]
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 
-
-class FeautureExtractor(object):
+class FeatureExtractor(object):
     GX = 16//2
     GY = 12//2
-    def __init__(self):
+
+    def __init__(self, K):
         self.orb = cv2.ORB_create(1000)
-        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING )
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING2)
         #self.bf = cv2.BFMatcher()
         self.last = None
-        self.seed = 9
-        self.rng = np.random.default_rng(self.seed)
-        
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+  
+    def denormalize(self, pt):
+        #print(self.Kinv)
+        ret = np.dot(self.K,[pt[0],pt[1],1])
+        #ret /= ret[2]
+        return (int(round(ret[0])), int(round(ret[1])))
+    
+    def isValid(self, model, data1, data2):
+        return model.matrix.shape[0] > 2  & model.matrix.shape[1] > 2
+
     def extract(self, img):
-        # Detection 
-        feats =  cv2.goodFeaturesToTrack(img, 3000, qualityLevel=0.01, minDistance=3)
-        # Encoding
+        # detection
+        feats = cv2.goodFeaturesToTrack(img, 3000, qualityLevel=0.01, minDistance=3)
+
+        # extraction
         kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in feats]
         kps, des = self.orb.compute(img, kps)
-        # Matching
+
+        # matching
         ret = []
-        if(self.last is not None):
-            #matches = self.bf.match(des, self.last['des'])
+        if self.last is not None:
             matches = self.bf.knnMatch(des, self.last['des'], k=2)
-            for m,n in matches: 
+            for m,n in matches:
                 if m.distance < 0.75*n.distance:
                     kp1 = kps[m.queryIdx].pt
                     kp2 = self.last['kps'][m.trainIdx].pt
-                    ret.append((kp1,kp2)) 
-            #matches = zip([kps[m.queryIdx] for m in matches], [self.last['kps'][m.trainIdx] for m in matches]) 
+                    ret.append((kp1, kp2))
 
-        self.last = {'kps': kps, 'des' : des}
         # filter
+        print("Ret length:", len(ret))
         if len(ret) > 0:
-            
             ret = np.array(ret)
-
-            tmp = (ret[:,0], ret[:,1])
-            print("Ret shape",ret.shape)
-            model, inliers = ransac((ret[:,0], ret[:,1]), FundamentalMatrixTransform,
-                                    min_samples=8, residual_threshold=0.01, max_trials=100, random_state=self.rng)
+            # Subtract to move to 0
+            #ret[:, :, 0] -= img.shape[0]//2
+            #ret[:, :, 1] -= img.shape[1]//2
+            ret[:, 0, :] = np.dot(self.Kinv, add_ones(ret[:,0,:]).T).T[:,0:2]
+            ret[:, 1, :] = np.dot(self.Kinv, add_ones(ret[:,1,:]).T).T[:,0:2]
+            model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                                    FundamentalMatrixTransform,
+                                    min_samples=8,
+                                    residual_threshold=1,
+                                    max_trials=100)
+            print("Inliers:", sum(inliers))  # Print inliers size 
+            ret = ret[inliers]
+            #print(model.params)
+            s,v,d = np.linalg.svd(model.params)
+            print(v)
         # return
+        self.last = {'kps': kps, 'des': des}
         return ret
+    
--- slam.py
+++ slam.py
@@ -1,33 +1,38 @@
 #/Library/Frameworks/Python.framework/Versions/3.12/bin/python3 python3
 import os
 import cv2
-import numpy
+import numpy as np
 import time
-from extractor import FeautureExtractor 
+from extractor import FeatureExtractor 
+import math
 
 
 W = 1920//2
 H = 1080//2
-
+#F = math.sqrt(2)/5.00688904e-3
+#F = math.sqrt(2)/3.84808079e-3
+F = 1
+K = np.array(([F,0,W//2],[0,F,H//2],[0,0,1]))
 orb = cv2.ORB_create()
 
 
-fe = FeautureExtractor()
- 
+fe = FeatureExtractor(K)
+
 def process_frame(img, frame):
     img = cv2.resize(img, (W,H))
+
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #fe.extract(imgray)
     print("Calculating keypoints")
     matches = fe.extract(imgray)
     if matches is not None:
         for p1, p2 in matches:
-            u1,v1 = map(lambda x: int(round(x)), p1)
-            u2,v2 = map(lambda x: int(round(x)), p2 )
-            cv2.circle(img, (u1,v1), color=(0,255,0), radius=3)
-            cv2.line(img, (u1,v1), (u2,v2), (255,0,0), thickness=2)
+            u1,v1 = fe.denormalize(p1)
+            u2,v2 = fe.denormalize(p2)
+            cv2.circle(img, (u1,v1), color=(0,255,0), radius=5)
+            cv2.line(img, (u1,v1), (u2,v2), (255,0,0), thickness=3)
     cv2.imshow(frame,img)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         exit()
 
