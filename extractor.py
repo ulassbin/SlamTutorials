f7a69a74389beaac50a7e1eacc7f0943fc980740
UlasBingol <e203316@metu.edu.tr>
2024-11-07 23:39:17 +0100

    Refactor to Frame class

--- extractor.py
+++ extractor.py
@@ -11,99 +11,98 @@ from skimage.transform import AffineTransform
 def add_ones(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
+def extractRt(self, E):
+    W =  np.mat([[0,-1,0],[1,0,0],[0,0,1]], dtype=float)
+    u,w,vt = np.linalg.svd(E)
+    assert np.linalg.det(u) > 0
+    if np.linalg.det(vt) < 0:
+        vt *= -1
+    # Retrieve the 2 rotations # Change of naming notation ad 2.14
+    R = np.dot(np.dot(u,W),vt) # Get the standard rotation matrix
+    if np.sum(np.diag(R)) < 0: # If the standard rotation matrix is negative
+        R = np.dot(np.dot(u,W.T),vt) # Get the other rotation matrix
+    #print("Rotation:", R)
+    # Get the translation from essential matrix
+    t = u[:,2]
+    #print("Translation:", t)
+    #print("Vt:", vt)
+    pose = np.concatenate([R,t.reshape(3,1)], axis=1)
+    return pose
 
-class FeatureExtractor(object):
-    GX = 16//2
-    GY = 12//2
+def normalize(Kinv, pt): # converts pixel coordinates to euclidian coordinates
+    return np.dot(Kinv, add_ones(pt).T).T[:,0:2] 
 
-    def extractRt(self, E):
-        W =  np.mat([[0,-1,0],[1,0,0],[0,0,1]], dtype=float)
-        u,w,vt = np.linalg.svd(E)
-        assert np.linalg.det(u) > 0
-        if np.linalg.det(vt) < 0:
-            vt *= -1
-        # Retrieve the 2 rotations # Change of naming notation ad 2.14
-        R = np.dot(np.dot(u,W),vt) # Get the standard rotation matrix
-        if np.sum(np.diag(R)) < 0: # If the standard rotation matrix is negative
-            R = np.dot(np.dot(u,W.T),vt) # Get the other rotation matrix
-        #print("Rotation:", R)
-        # Get the translation from essential matrix
-        t = u[:,2]
-        #print("Translation:", t)
-        #print("Vt:", vt)
-        pose = np.concatenate([R,t.reshape(3,1)], axis=1)
-        return pose
+def denormalize(K, pt): # converts euclidian coordinates to pixel coordinates
+    ret = np.dot(K,[pt[0],pt[1],1])
+    ret /= ret[2] # Normalize by the z coordinate (when F != 1)
+    return (int(round(ret[0])), int(round(ret[1])))
 
-    def __init__(self, K):
-        self.orb = cv2.ORB_create(1000) # orb keypoint container
-        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING2) # Brute force matcher
-        #self.bf = cv2.BFMatcher()
-        self.last = None
-        self.last_frame = None
+
+def extract(img):
+    orb = cv2.ORB_create(1000) # orb keypoint container
+    # detection
+    pts = cv2.goodFeaturesToTrack(img, 3000, qualityLevel=0.01, minDistance=3) # gets features in pixels right
+    # extraction
+    kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in pts] # converts features to cv2 keypoints
+    kps, des = orb.compute(img, kps) # computes the descriptors of the keypoints
+    return np.array([(kp.pt[0], kp.pt[1]) for kp in kps]), des
+
+
+def getPoseFromRet(ret, Kinv):
+    ret[:, 0, :] = normalize(Kinv, ret[:,0,:])
+    ret[:, 1, :] = normalize(Kinv, ret[:,1,:])
+    model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                            EssentialMatrixTransform, # less parameters
+                            #FundamentalMatrixTransform, # More paremeters
+                            min_samples=8,
+                            residual_threshold=0.005,
+                            max_trials=100) # We will take this matrix and extract positions
+    #ignore outliers
+
+
+
+
+    ret = ret[inliers]
+    #print(model.params) 
+    Rt = extractRt(model.params)
+    return Rt
+
+def match(f1, f2):
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    matches = bf.knnMatch(f1.des, f2.des, k=2) # Calls bruteforce knn matching with 2 nearest neighbours
+    
+    # matching
+    ret = []
+    good = []
+    # Lowes ratio test
+    for m,n in matches: # iterate through the matches
+        if m.distance < 0.75*n.distance: # Lowe's ratio test, checks if first match is significantly better than the second It is a method to filter!
+            p1 = f1.pts[m.queryIdx]
+            p2 = f2.pts[m.trainIdx]
+            ret.append((p1, p2))
+            good.append([m])
+    # cv.drawMatchesKnn expects list of lists as matches.
+    #if self.last is not None: # Nice visualizaton. YOU WERE HERE (VID TIMESTEP 1.54.27)
+    #    img3 = cv2.drawMatchesKnn(img,kps,img,self.last['kps'],good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    #    cv2.imshow('frame', img3)#,plt.show()
+    #    cv2.waitKey(1000)
+    # filter
+    #print("Ret length:", len(ret))
+    pose = None
+    assert len(ret) >= 8
+    ret = np.array(ret)
+    return ret
+
+
+
+class Frame(object):
+    def __init__(self, img, K):
         self.K = K
         self.Kinv = np.linalg.inv(self.K) 
-  
-    def normalize(self, pt): # converts pixel coordinates to euclidian coordinates
-        return np.dot(self.Kinv, add_ones(pt).T).T[:,0:2] 
-    
-    def denormalize(self, pt): # converts euclidian coordinates to pixel coordinates
-        #print(self.Kinv)
-        ret = np.dot(self.K,[pt[0],pt[1],1])
-        #print(ret)
-        ret /= ret[2] # Normalize by the z coordinate (when F != 1)
-        return (int(round(ret[0])), int(round(ret[1])))
-    
+        
+        self.pts, self.des = extract(img)
+        self.pts = normalize(self.Kinv, self.pts)
+        pass
+      
     def isValid(self, model, data1, data2):
         return model.matrix.shape[0] > 2  & model.matrix.shape[1] > 2
-
-    def extract(self, img, imgcolor):
-        # detection
-        feats = cv2.goodFeaturesToTrack(img, 3000, qualityLevel=0.01, minDistance=3) # gets features in pixels right
-        # extraction
-        kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in feats] # converts features to cv2 keypoints
-        kps, des = self.orb.compute(img, kps) # computes the descriptors of the keypoints
-
-        # matching
-        ret = []
-        good = []
-        if self.last is not None: # if there is a previous frame
-            matches = self.bf.knnMatch(des, self.last['des'], k=2) # Calls bruteforce knn matching with 2 nearest neighbours
-            for m,n in matches: # iterate through the matches
-                if m.distance < 0.75*n.distance: # Lowe's ratio test, checks if first match is significantly better than the second It is a method to filter!
-                    kp1 = kps[m.queryIdx].pt
-                    kp2 = self.last['kps'][m.trainIdx].pt
-                    ret.append((kp1, kp2))
-                    good.append([m])
-        # cv.drawMatchesKnn expects list of lists as matches.
-        #if self.last is not None: # Nice visualizaton. YOU WERE HERE (VID TIMESTEP 1.54.27)
-        #    img3 = cv2.drawMatchesKnn(img,kps,img,self.last['kps'],good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        #    cv2.imshow('frame', img3)#,plt.show()
-        #    cv2.waitKey(1000)
-        # filter
-        print("Ret length:", len(ret))
-        pose = None
-        if len(ret) > 0:
-            ret = np.array(ret)
-            # Subtract to move to 0
-            #ret[:, :, 0] -= img.shape[0]//2
-            #ret[:, :, 1] -= img.shape[1]//2
-            ret[:, 0, :] = self.normalize(ret[:,0,:])
-            ret[:, 1, :] = self.normalize(ret[:,1,:])
-            model, inliers = ransac((ret[:, 0], ret[:, 1]),
-                                    EssentialMatrixTransform, # less parameters
-                                    #FundamentalMatrixTransform, # More paremeters
-                                    min_samples=8,
-                                    residual_threshold=0.005,
-                                    max_trials=100) # We will take this matrix and extract positions
-            #print("Inliers:", sum(inliers))  # Print inliers size 
-            ret = ret[inliers]
-            #print(model.params)
-            W =  np.mat([[0,-1,0],[1,0,0],[0,0,1]], dtype=float)
-
-            pose = self.extractRt(model.params)
-
-        # return
-        self.last = {'kps': kps, 'des': des}
-        self.last_frame = img
-        return ret, pose  # Return the matches and the rotation and translation matrix
-    
--- slam.py
+++ slam.py
@@ -3,38 +3,39 @@ import os
 import cv2
 import numpy as np
 import time
-from extractor import FeatureExtractor 
+from extractor import *
 import math
 
+import g2o
 
+# Current time 2.30.15
+# Camera intrinsics
 W = 1920//2
 H = 1080//2
-#F = math.sqrt(2)/5.00688904e-3
-#F = math.sqrt(2)/3.84808079e-3
-F = 280
+F = 270
 K = np.array(([F,0,W//2],[0,F,H//2],[0,0,1]))
 orb = cv2.ORB_create()
 
 
-fe = FeatureExtractor(K)
-
+frames = []
 def process_frame(img, frame):
     img = cv2.resize(img, (W,H))
-
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #fe.extract(imgray)
-    #print("Calculating keypoints")
-    matches, pose = fe.extract(imgray, img) # here 
-    if pose is None:
+
+    frame = Frame(imgray, K)
+    frames.append(frame)
+
+    if (len(frames) <= 1 ):
         return
-    print(pose)
+    # else
+    matches = match(frames[-1], frames[-2])
     if matches is not None:
         for p1, p2 in matches:
-            u1,v1 = fe.denormalize(p1)
-            u2,v2 = fe.denormalize(p2)
-            cv2.circle(img, (u1,v1), color=(0,255,0), radius=5)
-            cv2.line(img, (u1,v1), (u2,v2), (255,0,0), thickness=3)
-    cv2.imshow(frame,img)
+            u1,v1 = denormalize(K, p1)
+            u2,v2 = denormalize(K, p2)
+            cv2.circle(img, (u1,v1), color=(0,255,0), radius=1)
+            cv2.line(img, (u1,v1), (u2,v2), (255,0,0), thickness=1)
+    cv2.imshow("frame",img)
     if cv2.waitKey(5) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         exit()
